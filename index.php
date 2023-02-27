076cf70f42e34bc02c2db1a7618ebc2173c5369b
Stanisław Kępiński <102586079+Kepinski-ei19@users.noreply.github.com>
2023-02-27 11:18:32 +0100

    Wrzucenie podstawowego szkieletu strony

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Stanisław Kępiński">
+    <title>Skouda to najlepsze auto</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <?php include 'struktura.php'?>
+
+    <main>
+        <article>
+            <h2>Coś nie działa....</h2>
+            Generalnie to od jakichś 3 dni walczę z immobilizerem, bo nie mogę zdobyć od niego PINu, by zaprogramować nowe kluczyki XD
+        </article>
+        <article>
+            <h2>Kupiłem dużo złomu!</h2>
+            Ziomek od którego nakupowałem gratów robi czyszczenie garażu i rozdaje części do felicii za friko, żyć nie umierać xD
+        </article>
+    </main>
+</body>
+</html>
---
+++ struktura.php
@@ -0,0 +1,12 @@
+<?php
+    echo '<header><h1>Blog Stahaa o jego przygodach motoryzacyjnych</h1></header>';
+
+    echo '<aside><ul id="menu">';
+    echo '<li><a href="index.php">Strona główna</a></li>';
+    echo '<li><a href="galeria.php">Galeria</a></li>';
+    echo '<li><a href="o_mnie.php">O Autorze</a></li>';
+    echo '<li><a href="kontakt.php">Kontakt</a></li>';
+    echo '</ul></aside>';
+
+    echo '<footer>&copy'.date("Y").' Stahoo Kępiński</footer>';
+?>

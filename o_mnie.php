f6422add5ab2c421e5dffe0147c021d8e382aefb
Kepinski-ei19 <kepinski.ei19@gmail.com>
2023-03-06 10:27:31 +0100

    set static header and footer heights, add sample text to pages

--- o_mnie.php
+++ o_mnie.php
@@ -12,7 +12,18 @@
     <?php include 'struktura.php'?>
 
     <main>
-        <article>Treść</article>
+        <article>
+            <h2>Header</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi deserunt minus officiis amet exercitationem libero, consequuntur obcaecati, accusantium mollitia harum corrupti fugit. Tenetur autem fugiat dignissimos, aliquid eum obcaecati consequatur.
+            Ducimus officia accusamus nesciunt voluptates blanditiis dignissimos maxime tempora eum voluptas, amet maiores repellendus velit distinctio nam quam suscipit. Dolorem eum ullam nulla, deserunt consectetur magnam. Ipsa voluptate ipsum dolorum!
+            Libero ex, sint, officiis suscipit doloremque modi quasi velit blanditiis hic neque, exercitationem obcaecati ut accusantium necessitatibus vel magnam. Beatae quo, nesciunt eligendi eaque dolorem neque nobis blanditiis itaque sint.
+        </article>
+        <article>
+            <h2>Header</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi deserunt minus officiis amet exercitationem libero, consequuntur obcaecati, accusantium mollitia harum corrupti fugit. Tenetur autem fugiat dignissimos, aliquid eum obcaecati consequatur.
+            Ducimus officia accusamus nesciunt voluptates blanditiis dignissimos maxime tempora eum voluptas, amet maiores repellendus velit distinctio nam quam suscipit. Dolorem eum ullam nulla, deserunt consectetur magnam. Ipsa voluptate ipsum dolorum!
+            Libero ex, sint, officiis suscipit doloremque modi quasi velit blanditiis hic neque, exercitationem obcaecati ut accusantium necessitatibus vel magnam. Beatae quo, nesciunt eligendi eaque dolorem neque nobis blanditiis itaque sint.
+        </article>
     </main>
 </body>
 </html>

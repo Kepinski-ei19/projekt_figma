92befadf91bfda17b105b1ae34e72cc5857fd2a0
Stanisław Kępiński <kepinskiei19@gmail.com>
2023-03-20 10:18:50 +0100

    add some content, resize layout

--- index.php
+++ index.php
@@ -14,6 +14,20 @@
 
         <main>
             <article>
+                <h2>Czas na powrót</h2>
+                Generalnie nie ma co opisywać, wystarczy spojrzeć do galerii :)
+            </article>
+            <article>
+                <h2>Mycie chrupka, malowanie błotników i tak dalej....</h2>
+                Przy okazji wizyty kolegi w sprawie felg, pożyczyłem sobie od niego garaż, gdzie pomalowałem nowe błotniki, by w końcu nie mieć dziury w aucie :D
+
+                Potem był czas na podróż z powrotem do domu...
+            </article>
+            <article>
+                <h2>Fela dostanie... fele :D</h2>
+                Byłem u kolegi oglądać felgi potencjalnie do kupienia, wydaje mi się, że spas może być jak należy. Można zobaczyć w galerii!
+            </article>
+            <article>
                 <h2>Coś nie działa....</h2>
                 Już zadziałało, gdyż jendak byłem zmuszony do wykodowania immobilisera totalnie z ECU, więc stacyjka w końcu siedzi w aucie.
             </article>

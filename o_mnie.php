9cbc35e226e224fa31bc51c407190001aa71551f
Stanisław Kępiński <kepinskiei19@gmail.com>
2023-03-27 10:17:38 +0200

    add link to my school's page

--- o_mnie.php
+++ o_mnie.php
@@ -18,6 +18,13 @@
                 Generalnie to jestem Stahó, uczeń 4 klasy technikum ZSTI na profilu Informatyk. Pobocznie interesuję się motoryzacją i ogarniam moje "ukochane" autko do porządku. Mam nadzieję, że jakoś mi to wyjdzie :D
             </article>
             <article>
+                <h2>Odnośnie ZSTI..</h2>
+                Trzymajcie linka! Wystarczy kliknąć w przycisk.<br>
+                <div class="link_container">
+                    <a href="https://zsti.gliwice.pl">Strona ZSTI</a>
+                </div>
+            </article>
+            <article>
                 <h2>Czym ja jeżdżę?</h2>
                 Niestety jeszcze nie jeżdżę, bo jestem dopiero w trakcie kursu na prawo jazdy, ale za to nabyłem całkiem niedawno Skodę "Felę" Felicię, która na początku była w opłakanym stanie. Na szczęście blacharka w tym aucie trzyma się w miarę dobrze, dlatego wybrałem to auto jako bazę na moje pierwsze auto z pierwszymi modyfikacjami. Generalnie to jest rocznik 1999, z 68-konnym 1.3 w gazie. Póki co służy jak należy.
             </article>

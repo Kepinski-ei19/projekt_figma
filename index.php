ca8e7ba066dfbf438dc296f7f673514141ade2f6
Stanisław Kępiński <kepinskiei19@gmail.com>
2023-04-03 10:15:12 +0200

    add some meta content so lighthouse is happy

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Stanisław Kępiński">
+    <meta name="description" content="Blog Stahaa, w którym wyżala się jak to felicia jest zajebistym autem XD">
     <title>Skouda to najlepsze auto</title>
     <link rel="stylesheet" href="style.css" />
 </head>
